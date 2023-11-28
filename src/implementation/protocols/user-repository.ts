ea88bd9181c5b5f291390fc4d4daf54b6d741e91
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-11-28 16:55:58 -0300

    feat: definindo metodos do user repository

--- src/implementation/protocols/user-repository.ts
+++ src/implementation/protocols/user-repository.ts
@@ -1,3 +1,8 @@
+import {User} from "../../domain/entities/User";
+
 export interface IUserRepository {
-  
+  register(data: User): Promise<User>
+  findAll(): Promise<User[]>
+  findOneByEmail(email: string): Promise<User | undefined>
+  findOneById(id: string): Promise<User | undefined>
 }
