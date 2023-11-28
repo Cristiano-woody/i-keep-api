e77dea7efcd9ca9070f09dbcbb1692eeac7d9dd7
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-11-28 16:58:31 -0300

    feat: criando implementacao do caso de uso de buscar usuario por email

--- src/implementation/use-cases/user/find-user-by-email.ts
+++ src/implementation/use-cases/user/find-user-by-email.ts
@@ -1,5 +1,11 @@
-import { User } from "../../entities/User";
+import { User } from '../../../domain/entities/User';
+import {IUserRepository} from "../../protocols/user-repository";
+import {IFindUserByEmailUseCase} from "../../../domain/use-cases/user/find-user-by-email";
 
-export interface IFindUserByEmailUseCase {
-  execute(email: string): Promise<User | undefined>
+export class FindUserByEmailUseCase implements IFindUserByEmailUseCase {
+  constructor(private userRepository: IUserRepository) {
+  }
+  async execute(email: string): Promise<User | undefined> {
+    return await this.userRepository.findOneByEmail(email)
+  }
 }
