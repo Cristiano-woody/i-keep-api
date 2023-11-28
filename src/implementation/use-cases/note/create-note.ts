13750e241f5ada6f8cb3401fe6321868ef04f556
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-11-28 16:56:35 -0300

    feat: criando implementacao do caso de uso de criar notas

--- src/implementation/use-cases/note/create-note.ts
+++ src/implementation/use-cases/note/create-note.ts
@@ -1,15 +1,27 @@
-export type createNoteUseCaseRequest = {
-  title: string
-  description: string
-  userId: string
-}
+import {
+  createNoteUseCaseRequest,
+  createNoteUseCaseResponse,
+  ICreateNoteUseCase
+} from "../../../domain/use-cases/note/create-note";
+import {INoteRepository} from "../../protocols/note-repository";
+import {Note} from "../../../domain/entities/Note";
+import {IUuidGenerate} from "../../helpers/uuid-generate";
 
-export type createNoteUseCaseResponse = {
-  title: string
-  description: string
-  id: string
-}
+export class CreateNoteUseCase implements  ICreateNoteUseCase{
+  constructor(private noteRepository: INoteRepository, private uuidGenerate: IUuidGenerate) {
+  }
 
-export interface ICreateNoteUseCase {
-  execute(data: createNoteUseCaseRequest): Promise<createNoteUseCaseResponse>
+  async execute(data: createNoteUseCaseRequest): Promise<createNoteUseCaseResponse> {
+    const newNote = new Note()
+    newNote.id = await this.uuidGenerate.execute()
+    newNote.title = data.title
+    newNote.description = data.description
+    newNote.user_id = data.userId
+    await this.noteRepository.create(newNote)
+    return ({
+      title: newNote.title,
+      description: newNote.description,
+      id: newNote.id
+    })
+  }
 }
