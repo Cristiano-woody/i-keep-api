da39cefea0c6d364be8de5e16f590a2ee8224b3f
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-11-28 16:57:39 -0300

    feat: criando implementacao do caso de uso de criar usuario

--- src/implementation/use-cases/user/create-user.ts
+++ src/implementation/use-cases/user/create-user.ts
@@ -1,19 +1,26 @@
-import { Note } from "../../entities/Note"
+import {createUserRequest, createUserResponse, ICreateUserUseCase} from "../../../domain/use-cases/user/create-user";
+import {IUserRepository} from "../../protocols/user-repository";
+import {User} from "../../../domain/entities/User";
+import {IUuidGenerate} from "../../helpers/uuid-generate";
+import {ICrypto} from "../../helpers/crypto";
 
-export type createUserRequest = {
-  name: string
-  email: string
-  password: string
-}
-
-export type createUserResponse = {
-  id: string
-  name: string
-  email: string
-  isActive: boolean;
-  notes?: Note[];
-}
-
-export interface ICreateUserUseCase {
-  execute(data: createUserRequest): Promise<createUserResponse>;
+export class CreateUserUseCase implements  ICreateUserUseCase {
+  constructor(private userRepository: IUserRepository, private uuidGenerate: IUuidGenerate, private crypto: ICrypto) {}
+  async execute(data: createUserRequest): Promise<createUserResponse> {
+    const newUser = new User()
+    newUser.id = await this.uuidGenerate.execute()
+    newUser.email = data.email
+    newUser.name = data.name
+    newUser.isActive = true
+    newUser.notes = []
+    newUser.password = await this.crypto.hash(data.password)
+    await this.userRepository.register(newUser)
+    return ({
+      id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      isActive: newUser.isActive,
+      notes: newUser.notes
+    })
+  }
 }
