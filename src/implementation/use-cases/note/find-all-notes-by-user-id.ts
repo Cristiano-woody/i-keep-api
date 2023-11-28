891dcbd4bc223ac94c0140ef67ab7dda08f11372
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-11-28 16:57:03 -0300

    feat: criando implementacao do caso de uso de bucar todas as notas por id do usuario

--- src/implementation/use-cases/note/find-all-notes-by-user-id.ts
+++ src/implementation/use-cases/note/find-all-notes-by-user-id.ts
@@ -1,5 +1,10 @@
-import { Note } from "../../entities/Note";
+import {IFindAllNotesByUserIdUseCase} from "../../../domain/use-cases/note/find-all-notes-by-user-id";
+import {INoteRepository} from "../../protocols/note-repository";
+import {Note} from "../../../domain/entities/Note";
 
-export interface IFindAllNotesByUserIdUseCase {
-  execute(userId: string): Promise<Note[]>
+export class FindAllNotesByUserIdUseCase implements  IFindAllNotesByUserIdUseCase{
+  constructor(private noteRepository: INoteRepository) {}
+  async execute(userId: string): Promise<Note[]> {
+    return await this.noteRepository.findAllByUserId(userId)
+  }
 }
