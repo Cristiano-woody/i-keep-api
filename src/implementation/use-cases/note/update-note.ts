8a455f0edca37674a78c536e9d6ed545fc62f6dc
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-11-28 21:29:00 -0300

    Refactor: adicionando validacao no caso de uso de atualizar nota

--- src/implementation/use-cases/note/update-note.ts
+++ src/implementation/use-cases/note/update-note.ts
@@ -1,11 +1,16 @@
-import { Note } from "../../entities/Note";
+import { IUpdateNoteUseCase, updateNoteUseCaseRequest } from "../../../domain/use-cases/note/update-note";
+import { Note } from "../../../domain/entities/Note";
+import { INoteRepository } from "../../protocols/note-repository";
+import { NoteNotFoundError } from "../../../domain/errors/note-not-found-error";
 
-export type updateNoteUseCaseRequest = {
-  title: string
-  description: string
-  id: string
-}
+export class UpdateNoteUseCase implements  IUpdateNoteUseCase {
+  constructor(private noteRepository: INoteRepository) {}
 
-export interface IUpdateNoteUseCase {
-  execute(data: updateNoteUseCaseRequest): Promise<Note>
+  execute(data: updateNoteUseCaseRequest): Promise<Note> {
+    const note = this.noteRepository.findOneById(data.id)
+    if(note == undefined) {
+      throw new NoteNotFoundError()
+    }
+    return this.noteRepository.update(data)
+  }
 }
