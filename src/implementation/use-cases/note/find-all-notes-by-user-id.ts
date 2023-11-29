fe87de8fb205b572d671838e5ee543dd68150e59
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-11-28 21:27:55 -0300

    refactor: adicionando validacaao no caso de uso de buscar notas pelo id do usuario

--- src/implementation/use-cases/note/find-all-notes-by-user-id.ts
+++ src/implementation/use-cases/note/find-all-notes-by-user-id.ts
@@ -1,10 +1,16 @@
 import {IFindAllNotesByUserIdUseCase} from "../../../domain/use-cases/note/find-all-notes-by-user-id";
 import {INoteRepository} from "../../protocols/note-repository";
 import {Note} from "../../../domain/entities/Note";
+import { UserNotFoundError } from "../../../domain/errors/user-not-found-error";
+import { IUserRepository } from "../../protocols/user-repository";
 
 export class FindAllNotesByUserIdUseCase implements  IFindAllNotesByUserIdUseCase{
-  constructor(private noteRepository: INoteRepository) {}
+  constructor(private noteRepository: INoteRepository, private userRepository: IUserRepository) {}
   async execute(userId: string): Promise<Note[]> {
+    const user = await this.userRepository.findOneById(userId)
+    if(user == undefined) {
+      throw new UserNotFoundError()
+    }
     return await this.noteRepository.findAllByUserId(userId)
   }
 }
