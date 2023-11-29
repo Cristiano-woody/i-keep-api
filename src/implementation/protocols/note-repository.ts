c2271b5cc833a0f4d1c68a2da5475647049a41ab
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-11-28 21:27:03 -0300

    feat: adicionando novos metodos no repositorio da nota

--- src/implementation/protocols/note-repository.ts
+++ src/implementation/protocols/note-repository.ts
@@ -4,6 +4,7 @@ export interface INoteRepository {
     create(data: Note): Promise<Note>
     findAllByUserId(userId: string): Promise<Note[]>
     findAll(): Promise<Note[]>
-    remove(): Promise<Note>
+    remove(noteId: string): Promise<Note>
     update(data: Partial<Note>): Promise<Note>
+    findOneById(noteId: string): Promise<Note | undefined>
 }
