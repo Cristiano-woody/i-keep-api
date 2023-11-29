34890f5056993c5aa4ab038de53d7d46a1a06f29
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-11-28 21:30:10 -0300

    refactor: adicionando validacao no caso de uso de buscar usuario por email

--- src/implementation/use-cases/user/find-user-by-email.ts
+++ src/implementation/use-cases/user/find-user-by-email.ts
@@ -1,11 +1,16 @@
 import { User } from '../../../domain/entities/User';
 import {IUserRepository} from "../../protocols/user-repository";
 import {IFindUserByEmailUseCase} from "../../../domain/use-cases/user/find-user-by-email";
+import { UserNotFoundError } from "../../../domain/errors/user-not-found-error";
 
 export class FindUserByEmailUseCase implements IFindUserByEmailUseCase {
   constructor(private userRepository: IUserRepository) {
   }
   async execute(email: string): Promise<User | undefined> {
-    return await this.userRepository.findOneByEmail(email)
+    const user = await this.userRepository.findOneByEmail(email)
+    if(!user) {
+      throw new UserNotFoundError()
+    }
+    return user
   }
 }
