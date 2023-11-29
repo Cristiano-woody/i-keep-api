23a0cabac282360a2e38fa4aa4f831b70e0c5b7e
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-11-28 21:27:26 -0300

    refactor: adicionando validacaao no caso de uso de criar nota

--- src/implementation/use-cases/note/create-note.ts
+++ src/implementation/use-cases/note/create-note.ts
@@ -6,12 +6,18 @@ import {
 import {INoteRepository} from "../../protocols/note-repository";
 import {Note} from "../../../domain/entities/Note";
 import {IUuidGenerate} from "../../helpers/uuid-generate";
+import { IUserRepository } from "../../protocols/user-repository";
+import { UserNotFoundError } from "../../../domain/errors/user-not-found-error";
 
 export class CreateNoteUseCase implements  ICreateNoteUseCase{
-  constructor(private noteRepository: INoteRepository, private uuidGenerate: IUuidGenerate) {
+  constructor(private noteRepository: INoteRepository, private uuidGenerate: IUuidGenerate, private userRepository: IUserRepository) {
   }
 
   async execute(data: createNoteUseCaseRequest): Promise<createNoteUseCaseResponse> {
+    const user = await this.userRepository.findOneById(data.userId)
+    if(user == undefined) {
+      throw new UserNotFoundError()
+    }
     const newNote = new Note()
     newNote.id = await this.uuidGenerate.execute()
     newNote.title = data.title
