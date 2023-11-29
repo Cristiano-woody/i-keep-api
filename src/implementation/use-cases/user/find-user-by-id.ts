6f687dbdf3acf3a5e534f6ed1b0b0d81e6683177
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-11-28 21:30:36 -0300

    refactor: adicionando validacao no caso de uso de buscar usuario por id

--- src/implementation/use-cases/user/find-user-by-id.ts
+++ src/implementation/use-cases/user/find-user-by-id.ts
@@ -1,11 +1,16 @@
 import { User } from '../../../domain/entities/User';
 import {IUserRepository} from "../../protocols/user-repository";
 import {IFindUserByIdUseCase} from "../../../domain/use-cases/user/find-user-by-id";
+import { UserNotFoundError } from "../../../domain/errors/user-not-found-error";
 
 export class FindUserByIdUseCase implements  IFindUserByIdUseCase{
   constructor(private userRepository: IUserRepository) {
   }
   async execute(id: string): Promise<User | undefined> {
-    return await this.userRepository.findOneById(id)
+    const user = await this.userRepository.findOneById(id)
+    if(!user) {
+      throw new UserNotFoundError()
+    }
+    return user
   }
 }
