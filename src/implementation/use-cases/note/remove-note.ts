b8332d84d55174310abb4959c400d10ad56cd419
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-11-28 21:28:17 -0300

    refactor: adicionando validacao no caso de uso de remover notaas

--- src/implementation/use-cases/note/remove-note.ts
+++ src/implementation/use-cases/note/remove-note.ts
@@ -1,3 +1,15 @@
-export interface IRemoveNoteUseCase {
-  execute(noteId: string): Promise<void>
+import { IRemoveNoteUseCase } from "../../../domain/use-cases/note/remove-note";
+import { INoteRepository } from "../../protocols/note-repository";
+import { NoteNotFoundError } from "../../../domain/errors/note-not-found-error";
+
+export class RemoveNoteUseCase implements IRemoveNoteUseCase {
+  constructor(private noteRepository: INoteRepository) {}
+
+  async execute(noteId: string): Promise<void> {
+    const note = this.noteRepository.findOneById(noteId)
+    if(note == undefined) {
+      throw new NoteNotFoundError()
+    }
+    await this.noteRepository.remove(noteId)
+  }
 }
