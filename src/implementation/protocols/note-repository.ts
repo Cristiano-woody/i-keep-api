7ab8bcb678d30eecaaaa14de86cac9d3ea3be7df
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-11-29 22:49:27 -0300

    refactor: atualizaando parametros dos metodos do repositorio de nota

--- src/implementation/protocols/note-repository.ts
+++ src/implementation/protocols/note-repository.ts
@@ -4,7 +4,7 @@ export interface INoteRepository {
     create(data: Note): Promise<Note>
     findAllByUserId(userId: string): Promise<Note[]>
     findAll(): Promise<Note[]>
-    remove(noteId: string): Promise<Note>
-    update(data: Partial<Note>): Promise<Note>
+    remove(note: Note): Promise<Note>
+    update(data: Partial<Note>, noteId: string): Promise<void>
     findOneById(noteId: string): Promise<Note | undefined>
 }
