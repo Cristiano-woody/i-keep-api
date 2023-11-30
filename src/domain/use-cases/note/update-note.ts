f6a307b7b02f6b2e06d3bbb2bb1e60bb7cd9de3c
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-11-29 22:48:52 -0300

    refactor: modificando parametros do caso de uso de atualizar nota

--- src/domain/use-cases/note/update-note.ts
+++ src/domain/use-cases/note/update-note.ts
@@ -3,5 +3,5 @@ import { Note } from "../../entities/Note";
 export type updateNoteUseCaseRequest = Partial<Note>
 
 export interface IUpdateNoteUseCase {
-  execute(data: updateNoteUseCaseRequest): Promise<Note>
+  execute(data: updateNoteUseCaseRequest, noteId: string): Promise<void>
 }
