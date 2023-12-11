f9d1c6fd9143c0221a53bada3c8a42d9e144045b
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-12-10 22:04:11 -0300

    test: criando teste para o caso de uso de remover nota

---
+++ test/use-cases/note/remove-note.spec.ts
@@ -0,0 +1,24 @@
+import { describe, beforeEach, it, expect } from "vitest";
+import { IRemoveNoteUseCase } from "../../../src/domain/use-cases/note/remove-note";
+import { INoteRepository } from "../../../src/implementation/protocols/note-repository";
+import { RemoveNoteUseCase } from "../../../src/implementation/use-cases/note/remove-note";
+import { NoteRepository } from "../../../src/infra/db/in-memory/note-repository";
+import { Note } from "../../../src/domain/entities/Note";
+import { NoteNotFoundError } from "../../../src/domain/errors/note-not-found-error";
+
+describe('', () => {
+  let sut: IRemoveNoteUseCase
+  let repo: INoteRepository
+  beforeEach(async () => {
+    repo = new NoteRepository()
+    sut = new RemoveNoteUseCase(repo)
+    await repo.create(new Note({title: "title", description: "description", user_id: "123", id: "1234"}))
+  });
+
+  it('should be able to remove note', async () => {
+    await expect(sut.execute("1234")).resolves.not.toThrow()
+  });
+  it('it should not be possible to remove the non-existent note', async () => {
+    await expect(sut.execute("000")).rejects.toThrow(new NoteNotFoundError())
+  })
+});
