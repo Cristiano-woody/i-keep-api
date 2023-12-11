88cd34270f79822bc9d21b933f6dc70900bebb6d
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-12-10 22:05:35 -0300

    test: criando teste para o caso de uso de buscar usuario por email

---
+++ test/use-cases/user/find-user-by-email.spec.ts
@@ -0,0 +1,36 @@
+import { describe, beforeEach, it, expect } from "vitest";
+import { IFindUserByEmailUseCase } from "../../../src/domain/use-cases/user/find-user-by-email";
+import { IUserRepository } from "../../../src/implementation/protocols/user-repository";
+import { UserRepository } from "../../../src/infra/db/in-memory/user-repository";
+import { FindUserByEmailUseCase } from "../../../src/implementation/use-cases/user/find-user-by-email";
+import { User } from "../../../src/domain/entities/User";
+import { UserNotFoundError } from "../../../src/domain/errors/user-not-found-error";
+
+describe('Test for find user by email use case.', () => {
+  let sut: IFindUserByEmailUseCase
+  let userRepo: IUserRepository
+  beforeEach(async () => {
+    userRepo = new UserRepository()
+    sut = new FindUserByEmailUseCase(userRepo)
+    await userRepo.register(new User({id: "333", name: "aa", email: "aa@sds.com", password: "123", isActive: true}))
+  });
+
+  it('should be able to get user by email.', async () => {
+    await expect(sut.execute("aa@sds.com")).resolves.not.toThrow()
+  });
+
+  it('should be able to get user by email.', async () => {
+    const user = await sut.execute("aa@sds.com")
+    expect(user).instanceof(User)
+  });
+
+  it('should be able to get user by email.', async () => {
+    const user = await sut.execute("aa@sds.com")
+    expect(user).haveOwnProperty("name")
+    expect(user.name).toEqual("aa")
+  });
+
+  it('should not be able to get user with invalid email.', async () => {
+    await expect(sut.execute("invalidEmail@invalid.com")).rejects.toThrow(new UserNotFoundError())
+  });
+});
