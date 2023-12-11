ecff49f213a12e608ac21c7606cbf9e3563b0e01
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-12-10 21:57:55 -0300

    refactor: modificando retorno do caso de uso de autualizar nota

--- src/domain/use-cases/note/update-note.ts
+++ src/domain/use-cases/note/update-note.ts
@@ -3,5 +3,5 @@ import { Note } from "../../entities/Note";
 export type updateNoteUseCaseRequest = Partial<Omit<Note, "id">>
 
 export interface IUpdateNoteUseCase {
-  execute(data: updateNoteUseCaseRequest, noteId: string): Promise<void>
+  execute(data: updateNoteUseCaseRequest, noteId: string): Promise<Note>
 }
