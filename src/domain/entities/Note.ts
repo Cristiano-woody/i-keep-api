1bb816ded3f58c576447ec8dd255a1b251f89746
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-12-10 21:57:01 -0300

    refactor: adicionando construtor na entidade Note

--- src/domain/entities/Note.ts
+++ src/domain/entities/Note.ts
@@ -3,4 +3,10 @@ export class Note {
   title: string
   description: string
   user_id: string
+  constructor(data?:Note) {
+    this.id = data.id
+    this.title = data.title
+    this.description = data.description
+    this.user_id = data.user_id
+  }
 }
