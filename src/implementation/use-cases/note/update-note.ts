32d1469a18b012eca00f37d490a33a302a57a564
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-12-10 21:58:36 -0300

    refactor: atualizando retorno na implementacao do caso de uso de atualizar nota

--- src/implementation/use-cases/note/update-note.ts
+++ src/implementation/use-cases/note/update-note.ts
@@ -6,11 +6,11 @@ import { NoteNotFoundError } from "../../../domain/errors/note-not-found-error";
 export class UpdateNoteUseCase implements  IUpdateNoteUseCase {
   constructor(private noteRepository: INoteRepository) {}
 
-  async execute(data: updateNoteUseCaseRequest, noteId: string): Promise<void> {
+  async execute(data: updateNoteUseCaseRequest, noteId: string): Promise<Note> {
     const note = await this.noteRepository.findOneById(noteId)
     if(note == undefined) {
       throw new NoteNotFoundError()
     }
-    await this.noteRepository.update(data, noteId)
+    return await this.noteRepository.update(data, noteId)
   }
 }
