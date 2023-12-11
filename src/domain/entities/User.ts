bee2db6abab222d1cef5664b77101e634b39d74e
cristiano nogueira <cristiano.nog.woody@gmail.com>
2023-12-10 21:57:28 -0300

    refactor: adicionando construtor na entidade usuario

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -7,4 +7,12 @@ export class User {
   password: string;
   isActive: boolean;
   notes?: Note[];
+  constructor(data?: User) {
+    this.id = data.id
+    this.name = data.name
+    this.email = data.email
+    this.password = data.password
+    this.isActive = data.isActive ?? true;
+    this.notes = data.notes
+  }
 }
