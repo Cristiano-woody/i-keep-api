6e3f57a25eeb9ba30f3edb9147354046deab96fc
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-03-06 18:07:02 -0300

    refactor: modificando interface do caso de uso de atualizar nota

--- src/domain/use-cases/note/update-note.ts
+++ src/domain/use-cases/note/update-note.ts
@@ -1,7 +1,7 @@
 import { Note } from "../../entities/Note";
 
-export type updateNoteUseCaseRequest = Partial<Omit<Note, "id">>
-
 export interface IUpdateNoteUseCase {
   execute(data: updateNoteUseCaseRequest, noteId: string): Promise<Note>
 }
+
+export type updateNoteUseCaseRequest = Partial<Omit<Note, "id">>
