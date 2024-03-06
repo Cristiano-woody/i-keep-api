a0bf0ad6dbffdda94cdf74486df8f834d8f3a030
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-03-06 18:00:26 -0300

    refactor: modificando entidade Note para usar padrao builder

--- src/domain/entities/Note.ts
+++ src/domain/entities/Note.ts
@@ -1,21 +1,12 @@
+import { NoteBuilder } from "./builders/NoteBuilder"
+
 export class Note {
   id: string
   title: string
   description: string
   user_id: string
-  private constructor(data?:Note) {
-    // usado por ORM
-    if(!data) {
-      this.id = ''
-      return
-    }
-    this.id = data.id
-    this.title = data.title
-    this.description = data.description
-    this.user_id = data.user_id
-  }
 
-  static create(data: Note) {
-    return new Note(data)
+  static builder() {
+    return new NoteBuilder()
   }
 }
